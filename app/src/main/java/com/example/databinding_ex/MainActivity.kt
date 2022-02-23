bcae28eb33b38be9f92b984752b5dbf06a323dce
Yeom <ygs01033@naver.com>
2022-02-24 01:05:05 +0900

    로그 추가

--- app/src/main/java/com/example/databinding_ex/MainActivity.kt
+++ app/src/main/java/com/example/databinding_ex/MainActivity.kt
@@ -2,19 +2,24 @@ package com.example.databinding_ex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.databinding_ex.databinding.ActivityMainBinding
 import com.example.databinding_ex.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+    // by lazy로 값을 선언 해준 후 ViewModelProvider로 ViewModel를 만든다
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this).get(MainViewModel::class.java)
+    }
     private lateinit var binding: ActivityMainBinding
-    private lateinit var viewModel: MainViewModel
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        // viewModel 생성
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
@@ -31,31 +36,31 @@ class MainActivity : AppCompatActivity() {
             viewModel.reset()
         }
 
-     //   observerViewModel()
+        observerViewModel()
 
     }
 
-/*    private fun observerViewModel(){
+    private fun observerViewModel(){
         // 좋아요 개수
-        viewModel.likeCount.observe(this, object : Observer<Int> {
-            override fun onChanged(t: Int?) {
-                Log.d("aaa", t.toString())
-           //     viewModel.likeCount.value.toString() = "${viewModel.likeCount.value}개"
+        viewModel.likeCount.observe(this,object : Observer<Int>{
+            override fun onChanged(likeCount: Int?) {
+                Log.d(TAG,"likeCount 변경 됨 likeCount 값 : $likeCount" )
             }
         })
-
         // 싫어요 개수
         viewModel.noLikeCount.observe(this, object : Observer<Int>{
-            override fun onChanged(t: Int?) {
-
+            override fun onChanged(noLikeCount: Int?) {
+                Log.d(TAG,"noLikeCount 변경 됨 noLikeCount 값 : $noLikeCount" )
             }
         })
 
         // 총 개수
         viewModel.totLikeCount.observe(this, object : Observer<Int>{
-            override fun onChanged(t: Int?) {
-
+            override fun onChanged(totLikeCount: Int?) {
+                Log.d(TAG,"totLikeCount 변경 됨 totLikeCount 값 : $totLikeCount" )
             }
         })
-    }*/
+    }
 }
+
+
